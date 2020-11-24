ae645b3bb0276408e731fd265f1bbe96b29de03a
Gharin Pautz <gharinpautz@gmail.com>
2020-11-23 20:14:55 -0800

    all view controllers added

---
+++ CPSC321Project/DestinationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DestinationTableViewCell.swift
+//  CPSC321Project
+//
+//  Created by Gharin Pautz on 11/23/20.
+//  Copyright © 2020 Gharin Pautz. All rights reserved.
+//
+
+import UIKit
+
+class DestinationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CPSC321Project/DestinationTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DestinationTableViewController.swift
+//  CPSC321Project
+//
+//  Created by Gharin Pautz on 11/23/20.
+//  Copyright © 2020 Gharin Pautz. All rights reserved.
+//
+
+import UIKit
+
+class DestinationTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
